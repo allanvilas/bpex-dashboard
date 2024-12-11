9c0ffc124e6461ee4c146dd3446837b112dc9fac
Allan Vilas Boas <allan.vilasboas@melius.software>
2024-12-11 11:43:42 -0300

    add adicionado rotas do projeto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/pages/dashboard', 'DashboardController::dashboardMainPage');
+$routes->get('/pages/login', 'LoginController::loginPage');
+$routes->get('/', 'LoginController::loginPage');
+$routes->post('/pages/login/auth', 'LoginController::authenticate');
+$routes->get('/pages/logout', 'LoginController::logout');
+
+// rotas de manipulacao de usuarios
+$routes->get('/pages/usuarios', 'UserController::userPage');
+$routes->get('/pages/usuarios/all', 'UserController::listarUsuarios');
+$routes->post('/pages/usuarios/create', 'UserController::criarNovoUsuario');
+$routes->post('/pages/usuarios/delete/(:num)', 'UserController::deletarUsuario/$1');
+$routes->put('/pages/usuarios/update/(:num)', 'UserController::atualizarUsuario/$1');
+$routes->post('/pages/usuarios/updatePassword', 'UserController::atualizarSenha');
